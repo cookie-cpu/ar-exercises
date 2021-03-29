98bc207cd13fae76096987825ae468ed13406643
Matthew Cook <matthewcook13987@gmail.com>
2021-03-29 20:23:17 +0000

    completed active record exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,20 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "joe", last_name: "bob", hourly_rate: 60)
+@store1.employees.create(first_name: "Firstina", last_name: "Lasta", hourly_rate: 60)
+
+@store2.employees.create(first_name: "bob", last_name: "joe", hourly_rate: 60)
+@store2.employees.create(first_name: "guy", last_name: "lastname", hourly_rate: 60)
+@store2.employees.create(first_name: "matthew", last_name: "cook", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employees < ActiveRecord::Base
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_inclusion_of :hourly_rate, in: 40..200
+  validates_presence_of :store_id
+end
+
+class Store < ActiveRecord::Base
+  validates_length_of :name, minimum: 3
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+end
+
+puts "Input a new store name!"
+@user_input = gets.chomp
+
+
+Store.create(name: @user_input)
